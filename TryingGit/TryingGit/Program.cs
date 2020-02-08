f4134e40f17d35641267141f333e32c78d45ddb6
AndrewK-ua <kravchukan1211@gmail.com>
2020-02-08 15:19:17 +0200

    first commit

---
+++ TryingGit/TryingGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryingGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
